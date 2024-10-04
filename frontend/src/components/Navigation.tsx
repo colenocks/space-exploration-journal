f5d0d4057bbc48f3fe2986e4bcaddf592f348811
colenocks <enockscoleman@gmail.com>
2024-10-04 20:58:15 +0100

    feat: highlight nav link for current page, add react-icons.

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,15 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FiHome, FiFileText } from "react-icons/fi";
 
 const Navigation = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
-    <div className='bg-gray-900 mx-1 rounded-lg text-gray-100 h-full p-5 shadow-xl'>
-      <h2 className='text-2xl mb-6 text-center font-bold'>Dashboard</h2>
+    <div className='bg-gray-900 mx-1 rounded-lg text-gray-100 h-full w-60 p-5 shadow-xl'>
+      <h2 className='text-2xl mb-6 text-left font-bold'>Dashboard</h2>
       <ul className='space-y-3'>
-        <li className='text-gray-100 hover:text-cyan-400 transition-colors duration-300 hover:cursor-pointer py-0.5 px-1'>
-          <Link to='/'>Home</Link>
+        <li className=''>
+          <Link
+            to='/'
+            className={`flex items-center transition-colors duration-300 ${isActive("/") ? "text-cyan-600" : "text-gray-100 hover:text-cyan-400"}`}>
+            <FiHome size={20} />
+            <span className={`ml-3 mt-0.5 transition-opacity duration-300`}>Home</span>
+          </Link>
         </li>
-        <li className='text-gray-100 hover:text-cyan-400 transition-colors duration-300 hover:cursor-pointer py-0.5 px-1'>
-          <Link to='/page1'>Page 1</Link>
+        <li className=''>
+          <Link
+            to='/page1'
+            className={`flex items-center transition-colors duration-300 ${
+              isActive("/page1") ? "text-cyan-600" : "text-gray-100 hover:text-cyan-400"
+            }`}>
+            <FiFileText size={20} />
+            <span className={`ml-3 mt-0.5 transition-opacity duration-300`}>Page 1</span>
+          </Link>
         </li>
       </ul>
     </div>
