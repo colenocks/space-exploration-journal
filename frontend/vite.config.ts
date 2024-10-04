2745698b2be7691a715c0c4087521b0d5195a315
colenocks <enockscoleman@gmail.com>
2024-10-05 00:20:10 +0100

    feat: install shadcn/ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path" // 'npm i -D @types/node' helps import path without error
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
