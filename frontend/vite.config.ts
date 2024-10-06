f06f42e1cb9dc2b8ad349a08a88d3de9dcf00e77
colenocks <enockscoleman@gmail.com>
2024-10-06 19:02:34 +0100

    chore: configure frontend to communicate with backend server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,42 @@
 import path from "path" // 'npm i -D @types/node' helps import path without error
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig((env)=>{
+  const envars = loadEnv(env.mode, './');
+
+  const serverURL = new URL(
+    envars.SERVER_URL ?? '<http://localhost:5000>'
+  );
+  const serverAPIPath = process.env.SERVER_API_PATH ?? "/api";
+  
+  return {
+      envDir: './',
+
+      // Make the API path globally available in the frontend
+      define: {
+        __API_PATH__: JSON.stringify(serverAPIPath),
+      },
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+
+      server: {
+        port: 5000,
+
+        /* 
+        The proxy setting enables communication between the frontend and the backend. 
+        Requests matching the API path are forwarded to the server URL. 
+        When we send a request to http://localhost:5000/api it will be forwarded 
+        to our server at http://localhost:3001/api.
+         */
+        proxy: {
+          [serverAPIPath]: serverURL.origin,
+        }
+      },
+    }
 })
