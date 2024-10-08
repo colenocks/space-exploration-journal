f416797990e9895743bf363dd3e40b1ad498da43
colenocks <enockscoleman@gmail.com>
2024-10-08 23:11:49 +0100

    feat: add time travel button

--- frontend/src/pages/PlanTrip.tsx
+++ frontend/src/pages/PlanTrip.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { FaRocket } from "react-icons/fa6";
+import { FaRocket, FaTimeline } from "react-icons/fa6";
 import LaunchAnimation from "@/components/LaunchAnimation";
 import Image from "@/components/Image";
 import { useRandomItemSelector } from "@/lib/useRandomSelector";
@@ -32,6 +32,9 @@ async function fetchAPODImages(count?: number) {
   return json;
 }
 
+// Helper function to add delay
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 const PlanTrip = () => {
   const [isLaunching, setIsLaunching] = useState(false);
 
@@ -54,8 +57,26 @@ const PlanTrip = () => {
 
   const simulateLaunch = async () => {
     setIsLaunching(true);
-    const journalData: IJournal[] = [];
     const updatedMonthlyData = { ...monthlyData };
+
+    const journalData = await populateMonthlyData(selectedPlanets);
+
+    updatedMonthlyData[MONTHS[currentMonth]] = journalData;
+    setMonthlyData(updatedMonthlyData);
+
+    // Move to next month or end the year
+    if (currentMonth === MONTHS.length - 1) {
+      setYearCompleted(true); // End of the year
+    } else {
+      setCurrentMonth(prevMonth => prevMonth + 1);
+    }
+
+    setIsLaunching(false);
+    clearSelection();
+  };
+
+  const populateMonthlyData = async (selectedPlanets: IPlanet[]) => {
+    const journalData: IJournal[] = [];
 
     for (const planet of selectedPlanets) {
       const tripDate = new Date().toLocaleString();
@@ -63,7 +84,6 @@ const PlanTrip = () => {
 
       try {
         const apodImages = await fetchAPODImages(number);
-
         // Push the journal entry for the current planet
         journalData.push({
           planet: planet.name,
@@ -75,19 +95,7 @@ const PlanTrip = () => {
         console.error("Failed to fetch APOD images:", error);
       }
     }
-
-    updatedMonthlyData[MONTHS[currentMonth]] = journalData;
-    setMonthlyData(updatedMonthlyData);
-
-    // Move to next month or end the year
-    if (currentMonth === 11) {
-      setYearCompleted(true); // End of the year
-    } else {
-      setCurrentMonth(prevMonth => prevMonth + 1);
-    }
-
-    setIsLaunching(false);
-    clearSelection();
+    return journalData;
   };
 
   useEffect(() => {
@@ -97,9 +105,9 @@ const PlanTrip = () => {
     localStorage.setItem("journal", JSON.stringify(monthlyData));
   }, [monthlyData, yearCompleted]);
 
-  const handleLaunchClick = () => {
+  const handleLaunchClick = async () => {
     if (selectedPlanets.length > 0) {
-      simulateLaunch();
+      await simulateLaunch();
     }
   };
 
@@ -107,22 +115,52 @@ const PlanTrip = () => {
     selectRandomPlanets();
   };
 
+  const [travelling, setTravelling] = useState(false);
+  useEffect(() => {
+    if (travelling) {
+      const runAsyncFunctionWithDelay = async () => {
+        for (let i = 0; i < 11; i++) {
+          selectRandomPlanets();
+          await delay(200); // add delay for asynchronous calls
+          await simulateLaunch();
+        }
+      };
+
+      runAsyncFunctionWithDelay()
+        .then(() => {
+          setTravelling(false);
+        })
+        .catch(error => {
+          console.error("Error during async execution:", error);
+          setTravelling(false);
+        });
+    }
+  }, [travelling]);
+
+  const handleTimeTravel = () => {
+    setCurrentMonth(0);
+    setMonthlyData({});
+    setTravelling(true);
+  };
+
   return (
     <div className='flex flex-col items-center'>
-      <h1 className='text-3xl font-bold text-white mb-12'>Plan Your Monthly Space Trips</h1>
+      <h1 className='text-3xl font-bold text-white'>Plan Your Monthly Space Trips</h1>
+
+      <small className='text-center mt-4 mb-12'>Click select planets and launch to visit per month until end of the yea.r</small>
 
       {!isLaunching ? (
         <div className='text-center'>
           <button
             className='bg-gray-700 hover:cursor-pointer capitalize hover:text-pink-400 text-white px-4 py-2 font-semibold rounded-md'
             onClick={handleSelectPlanets}>
-            Click to generate planets to visit
+            Select Planets
           </button>
           <div className='mt-5 text-sm'>
             Current Month: <strong className='text-pink-400'>{MONTHS[currentMonth]}</strong>
           </div>
 
-          <div className='flex flex-col gap-3 mt-4 mb-12'>
+          <div className='flex flex-col gap-3 mt-4 mb-6'>
             {selectedPlanets.length > 0 && (
               <div className='flex flex-col gap-5 justify-center items-center'>
                 <div className='flex flex-wrap items-center justify-center max-w-90 text-center gap-2'>
@@ -146,23 +184,34 @@ const PlanTrip = () => {
       )}
       <button
         onClick={handleLaunchClick}
-        className={`bg-cyan-700 text-white px-6 py-2 rounded-md flex items-center space-x-2 ${isLaunching ? "opacity-50" : ""}`}
+        className={`bg-cyan-700 hover:bg-cyan-500 text-white px-6 py-2 rounded-md flex items-center space-x-2 ${isLaunching ? "opacity-50" : ""}`}
         disabled={isLaunching}>
         <FaRocket />
         <span>{isLaunching ? "Launching..." : "Launch Rocket"}</span>
       </button>
 
+      <small className='text-center mt-12'>Or click to time travel through the monthly visits for the whole year.</small>
+      <button
+        onClick={handleTimeTravel}
+        className={`bg-pink-900 text-white px-6 py-2 mt-2 hover:bg-pink-500 rounded-md flex items-center space-x-2 ${
+          isLaunching ? "opacity-50" : ""
+        }`}
+        disabled={isLaunching}>
+        <FaTimeline />
+        <span>{isLaunching ? "Travelling Through Time" : "Time Travel"}</span>
+      </button>
+
       {/* TODO: Move journal entries to a separate file */}
       {yearCompleted && monthlyData && Object.keys(monthlyData).length > 0 && (
         <div className='mt-20 w-full space-y-6 max-w-2xl'>
-          <h2 className='mt-5 text-sm text-pink-400'>Data has been stored for Visualizations. Go to Visualize Trips</h2>
-          {Object.values(monthlyData).map(monthJournal => {
+          <h2 className='mt-5 text-sm text-pink-400'>Data has been stored for Visualizations. Go to Space Trips Charts</h2>
+          {Object.values(monthlyData).map((monthJournal, index) => {
             return (
-              <div>
-                <h2 className='text-lg font-bold text-cyan-700 mb-1'>Space Journal for {MONTHS[currentMonth - 1]}</h2>
+              <div key={MONTHS[index]}>
+                <h2 className='text-lg font-bold text-cyan-700 mb-1'>Space Journal for {MONTHS[index]}</h2>
                 <ul className='text-white'>
-                  {monthJournal.map((entry, index) => (
-                    <li key={entry.planet + index} className='border-b border-gray-700 py-2'>
+                  {monthJournal.map((entry, i) => (
+                    <li key={entry.planet + i} className='border-b border-gray-700 py-2'>
                       <div>
                         Trip to <strong>{entry.planet}</strong> - {entry.tripDate}
                       </div>
@@ -185,7 +234,6 @@ const PlanTrip = () => {
               </div>
             );
           })}
-          ;
         </div>
       )}
     </div>
