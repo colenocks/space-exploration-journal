8be9473a9cdef7ac3b364f9f223ca318d9b3d2ed
colenocks <enockscoleman@gmail.com>
2024-10-09 21:48:35 +0100

    style: adjust border radius and padding between nav and main content

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,15 +4,15 @@ import Navigation from "./Navigation";
 
 const Dashboard = () => {
   return (
-    <div className='flex h-screen py-2 bg-gray-850 p-1'>
+    <div className='flex h-screen py-1 bg-gray-850 p-0.5'>
       <Navigation />
 
       <div className='flex-grow flex flex-col'>
-        <div className='top-2 w-full lg:px-40 z-10 rounded-lg bg-gray-900 px-10 pt-6'>
+        <div className='top-2 w-full lg:px-40 z-10 rounded-tl-lg rounded-tr-lg bg-gray-900 px-10 pt-6'>
           <Header title='Welcome, Space Explorer' />
         </div>
 
-        <div className='flex-grow px-10 py-6 lg:px-40 bg-gray-900 text-gray-100 overflow-y-auto rounded-lg'>
+        <div className='flex-grow px-10 py-6 lg:px-40 bg-gray-900 text-gray-100 overflow-y-auto rounded-bl-lg rounded-br-lg'>
           <Outlet />
         </div>
       </div>
