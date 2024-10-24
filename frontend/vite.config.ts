06f915a55515de23f34e8dbefc8dec254e874229
colenocks <enockscoleman@gmail.com>
2024-10-24 19:39:25 +0100

    fix: process.env is always undefined in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,37 +1,42 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const serverURL = new URL(
-  process.env.VITE_BACKEND_URL ?? 'http://localhost:5000'
-);
-const serverAPIPath = process.env.VITE_SERVER_API_PATH ?? "/api";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-      envDir: './',
-      base: './',
-      plugins: [react()],
-      build: {
-        outDir: "../backend/public",
-      },
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
-        },
-      },
-      server: {
-        port: Number(process.env.PORT) ?? 3001,
+export default defineConfig((env) => {
+  const envars = loadEnv(env.mode, './');
 
-        /* 
-        The proxy setting enables communication between the frontend and the backend. 
-        Requests matching the API path are forwarded to the server URL. 
-        When we send a request to http://localhost:5000/api it will be forwarded 
-        to our server at http://localhost:3001/api.
-         */
-        proxy: {
-          [serverAPIPath]: serverURL.origin,
-        }
+  console.log(envars.VITE_BACKEND_URL)
+
+  const serverURL = new URL(
+    envars.VITE_BACKEND_URL ?? 'http://localhost:5000'
+  );
+  const serverAPIPath = envars.VITE_SERVER_API_PATH ?? '/api';
+
+  return {
+    envDir: './',
+    base: './',
+    plugins: [react()],
+    build: {
+      outDir: "../backend/public",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }
-)
+    },
+    server: {
+      port: Number(process.env.PORT) ?? 3001,
+
+      /* 
+      The proxy setting enables communication between the frontend and the backend. 
+      Requests matching the API path are forwarded to the server URL. 
+      When we send a request to http://localhost:5000/api it will be forwarded 
+      to our server at http://localhost:3001/api.
+       */
+      proxy: {
+        [serverAPIPath]: serverURL.origin,
+      }
+    },
+  }
+})
